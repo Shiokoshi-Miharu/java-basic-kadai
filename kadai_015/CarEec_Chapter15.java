927073377323c9d24be52d1b1df5c28cc0254d55
Shiokoshi-Miharu <shiokoshi.miharu.1207@gmail.com>
2024-10-06 12:05:42 +0900

    課題15を提出

---
+++ kadai_015/CarEec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarEec_Chapter15 {
+
+	
+	//車クラスのインスタンスを作成する
+	public static void main(String[] args) {
+		
+		Car_Chapter15 car = new Car_Chapter15();
+		
+		car.gearChange(3);
+			car.run();
+	}
+	
+	
+}
