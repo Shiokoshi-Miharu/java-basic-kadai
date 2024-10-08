668e6632a15dc83429ee8633182cefdc7a177a6c
Shiokoshi-Miharu <shiokoshi.miharu.1207@gmail.com>
2024-10-09 00:22:31 +0900

    Car_Chapter15.javaを修正

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -9,29 +9,23 @@ public class Car_Chapter15 {
 	private int gear = 1; //1速から5速のギアを表す
 	private int speed = 10; //ギアチェンジ後の速度を表す
 
-	//メソッド
-	public void CarChapter15(int gear, int speed) {
-		this.gear = gear;
-		this.speed = speed;
-	}
-	
 	//ギアに応じた速度
 	public void gearChange(int afterGear) {
+		int previousGear = this.gear;
 		this.gear = afterGear;
 		this.speed = switch(this.gear) {
-		case 1 -> this.speed = 10;
-		case 2 -> this.speed = 20;
-		case 3 -> this.speed = 30;
-		case 4 -> this.speed = 40;
-		case 5 -> this.speed = 50;
-		default -> this.speed = 10;
-		};
-	
+			case 1 -> this.speed = 10;
+			case 2 -> this.speed = 20;
+			case 3 -> this.speed = 30;
+			case 4 -> this.speed = 40;
+			case 5 -> this.speed = 50;
+			default -> this.speed = 10;
+			};
+		System.out.println("ギア" + previousGear + "から" + this.gear + "に切り替えられました");
 	}
 	
 	//runメソッド
 	public void run() {
-		System.out.println("ギア1から" + this.gear + "に切り替えられました");
 		System.out.println("速度は時速" + this.speed + "kmです");
 	}
 }
